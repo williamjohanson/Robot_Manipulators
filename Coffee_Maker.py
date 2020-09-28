cd446c2b7043967c27614e4ed3fe68f4f9681c22
liamhare <liam.hare@icloud.coim>
2020-09-28 18:18:09 +1300

    "stuff is moving"

--- Coffee_Maker.py
+++ Coffee_Maker.py
@@ -36,35 +36,53 @@ J_intermediatePortafilter = [-156.440056, -81.180555, -75.398392, -113.543732, 8
 
 #Transform to Portafilter Tool
 #First need to spin TCP 50degrees using following transform
-#T_TCP__np = np.array([[ np.cos(theta_T_GT_TCP), -np.sin(theta_T_GT_TCP), 0.00,    0.00],
-#                        [ np.sin(theta_T_GT_TCP),  np.cos(theta_T_GT_TCP), 0.00,    0.00],
-#                        [                   0.00,                    0.00, 1.00,    0.00],
-#                        [                   0.00,                    0.00, 0.00,    1.00]])
-#theta_T_TCP_PFT = np.radians(7.5)
-#T_TCP_PFT_np = np.array([[
+theta_CL_PFA = np.radians(50) #change 50 degree angle before moving 7.5 degrees
+T_TCP_PFA_np = np.array([[ np.cos(theta_CL_PFA), -np.sin(theta_CL_PFA), 0.00,    0.00],
+                     [ np.sin(theta_CL_PFA),  np.cos(theta_CL_PFA), 0.00,    0.00],
+                     [                   0.00,                    0.00, 1.00,    0.00],
+                     [                   0.00,                    0.00, 0.00,    1.00]])
+theta_T_TCP_PFT = np.radians(7.5)
+T_TCP_PFT_np = np.array([[np.cos(theta_T_TCP_PFT ), 0.00, np.sin(theta_T_TCP_PFT ), 32.00],
+                         [    0.00, 1.00,   0.00, 0.00],
+                         [-np.sin(theta_T_TCP_PFT ), 0.00,  np.cos(theta_T_TCP_PFT ), -27.56],
+                         [    0.00, 0.00,   0.00,    1.00]])
+
+T_compensation_np = np.array([[ 1.000000, 0.000000, 0.000000,   0.00],
+                              [ 0.000000, 1.000000,  0.000000,  0.00000],
+                              [ 0.000000, 0.000000,  1.000000,  0.00],
+                              [ 0.000000, 0.000000,  0.000000, 1.000000]])
+T_CL_PFT_np = np.matmul(T_TCP_PFT_np, np.matmul(T_TCP_PFA_np, T_compensation_np))  
+#Grinder Matrices
+theta_grinder_machine = np.radians(44.79)
+T_TCP_GR_np = np.array([[ -np.cos(theta_grinder_machine), -np.sin(theta_grinder_machine),  0.000000,  482.29],
+                             [ np.sin(theta_grinder_machine),  -np.cos(theta_grinder_machine),  0.000000,  -433.74],
+                             [                      0.000000,                       0.000000,  1.000000,   314.13],
+                             [                      0.000000,                       0.000000,  0.000000, 1.000000]])
+
+#Transfrom from grinder local frame to Portafilterplacement (PFP)
+T_GR_PFP_np = np.array([[ 0.000000, 0.000000, -1.000000,   157.61],
+                        [ 0.000000, 1.000000,  0.000000,  0.00000],
+                        [ 1.000000, 0.000000,  0.000000,  -250.45],
+                        [ 0.000000, 0.000000,  0.000000, 1.000000]])
 
 #Transform to the Grinding Tool
-theta_T_GT_TCP = np.radians(50)
-T_TCP_GT_np = np.array([[ np.cos(theta_T_GT_TCP), -np.sin(theta_T_GT_TCP), 0.00,    0.00],
-                        [ np.sin(theta_T_GT_TCP),  np.cos(theta_T_GT_TCP), 0.00,    0.00],
-                        [                   0.00,                    0.00, 1.00, -102.82],
-                        [                   0.00,                    0.00, 0.00,    1.00]])
+theta_CL_GT = np.radians(50)
+T_TCP_GT_np = np.array([[ np.cos(theta_CL_GT), -np.sin(theta_CL_GT), 0.00,    0.00],
+                        [ np.sin(theta_CL_GT),  np.cos(theta_CL_GT), 0.00,    0.00],
+                        [                0.00,                 0.00, 1.00, -102.82],
+                        [                0.00,                 0.00, 0.00,    1.00]])
 #Coffee Machine Matrices
 theta_coffee_machine = np.radians(105.038)
 T_TCP_CMBase_np = np.array([[ np.cos(theta_coffee_machine), -np.sin(theta_coffee_machine),  0.000000,  -366.20],
-                                   [ np.sin(theta_coffee_machine),  np.cos(theta_coffee_machine),  0.000000,  -389.80],
-                                   [                     0.000000,                      0.000000,  1.000000,   341.38],
-                                   [                     0.000000,                      0.000000,  0.000000, 1.000000]])
+                            [ np.sin(theta_coffee_machine),  np.cos(theta_coffee_machine),  0.000000,  -389.80],
+                            [                     0.000000,                      0.000000,  1.000000,   341.38],
+                            [                     0.000000,                      0.000000,  0.000000, 1.000000]])
 #Rotation matrix from coffee machine to the buttons
 R_CM_B_np = np.array([[ 0.00, 0.00,-1.00],
                       [-1.00, 0.00, 0.00],
                       [ 0.00, 1.00, 0.00]])
 
-theta_grinder_machine = np.radians(44.79)
-T_grinderBase_np = np.array([[ -np.cos(theta_grinder_machine), -np.sin(theta_grinder_machine),  0.000000,  482.29],
-                             [ np.sin(theta_grinder_machine),  -np.cos(theta_grinder_machine),  0.000000,  -433.74],
-                             [                      0.000000,                       0.000000,  1.000000,   314.13],
-                             [                      0.000000,                       0.000000,  0.000000, 1.000000]])
+
 
 theta_press = np.radians(59.73)
 T_pressBase_np = np.array([[ np.cos(theta_press), -np.sin(theta_press),  0.000000,  370.66],
@@ -72,11 +90,7 @@ T_pressBase_np = np.array([[ np.cos(theta_press), -np.sin(theta_press),  0.00000
                         [            0.000000,             0.000000,  1.000000,   66.86],
                         [            0.000000,             0.000000,  0.000000, 1.000000]])
 
-theta_portafilterPlace = np.radians(50)
-T_grinderPortaPlaceRG_np = np.array([[ 0.000000, np.sin(theta_portafilterPlace), np.cos(theta_portafilterPlace),   157.61],
-                                     [ 0.000000, np.cos(theta_portafilterPlace),-np.sin(theta_portafilterPlace),  0.00000],
-                                     [-1.000000,                       0.000000,                       0.000000,  -250.45],
-                                     [ 0.000000,                       0.000000,                       0.000000, 1.000000]])
+
 
 def coffeeMachineButtons():
    
@@ -120,24 +134,52 @@ def coffeeMachineButtons():
    robot.MoveL(T_GT_B1Set, blocking=True)
    robot.MoveL(T_GT_B1Push, blocking=True) 
    robot.MoveJ(T_GT_B1Set, blocking=True)
+   robot.MoveJ(J_intermediateGrinderTool, blocking=True)
+   RDK.RunProgram('Grinder Tool Detach (Stand)', True)
    #robot.MoveJ(J_intermediatePointPusher, blocking=True)
    #RDK.RunProgram('Grinder Tool Detach (Stand)', True)
    
 
-coffeeMachineButtons()
 
-#def portafilterPlacement():
+
+def portafilterPlacement():
+   #Calculate placement position for portafilter under the grinder
+   T_TCP_PFP_np = np.matmul(T_TCP_GR_np, np.matmul(T_GR_PFP_np, T_CL_PFT_np)) 
    
+   T_TCP_PFP = rdk.Mat(T_TCP_PFP_np.tolist())
+   T_TCP_GR  = rdk.Mat(T_TCP_GR_np.tolist())
+   
+   #Steps from tools tand to Grinder
+   J_Step1 = [-156.440000, -81.180000, -75.390000, -181.780000, 181.780000, -182.170000]
+   J_Step2 = [-156.440000, -81.180000, -127.130000, -181.780000, 181.780000, -182.170000]
+   J_Step3 = [-71.050000, -81.180000, -127.130000, -136.630000, 181.780000, -182.170000]
+   J_Step4 = [-14.210000, -65.350000, -148.510000, -148.510000, 293.470000, -231.680000]
+   J_Step5 = [-17.818290, -100.202192, -139.887909, -111.506940, 296.722534, -223.800304]
+   robot.MoveJ(J_Home, blocking=True)
+   robot.MoveJ(J_intermediatePortafilter, blocking=True)
+   RDK.RunProgram('Portafilter Tool Attach (Stand)', True)
+   robot.MoveJ(J_Step1, blocking=True)
+   robot.MoveJ(J_Step2, blocking=True)
+   robot.MoveJ(J_Step3, blocking=True)
+   robot.MoveJ(J_Step4, blocking=True)
+   robot.MoveJ(J_Step5, blocking=True)
+   #robot.MoveJ(T_TCP_GR, blocking=True)
+   robot.MoveL(T_TCP_PFP, blocking=True)
+   RDK.RunProgram('Portafilter Tool Detach (Grinder)', True)
+   
+   #go and grab the Grinder tool
+   #RDK.RunProgram('Portafilter Tool Attach (Grinder)', True)
+   robot.MoveJ(J_intermediateGrinderTool, blocking=True)
+   RDK.RunProgram('Grinder Tool Attach (Stand)', True)
+   
+   
+   
+#coffeeMachineButtons()
+portafilterPlacement()
 
-T_grinderPortaPlace_np = np.dot( T_grinderBase_np, T_grinderPortaPlaceRG_np)
+
 
    
-
-#Tryimg to get the portafilter over to the grinder
-T_portafilterCallPoint_np = np.array([[ 0.000000, np.sin(50),  np.cos(50),  157.61],
-                                   [ 0.000000, np.cos(50), -np.sin(50), 0],
-                                   [ -1.00000,  0.0000000,  0.00000000,   -250.45],
-                                   [ 0.000000,  0.0000000,  0.00000000,   1.000]])
 
 #portaPosition = np.array([1.00, 1.00, 1.00, 1.00])
 grinder_base_position_np = np.array([0, 50, -50, 1])
@@ -149,7 +191,6 @@ T_portafilterCallPoint_np = [328.78, -398.04, 63.68, 0, 0, 0]
 grinder_base_position = rdk.Mat(grinder_base_position_np)
 T_grinderToolApproach = rdk.Mat(T_grinderToolApproach_np.tolist())
 T_TCP_CMBase = rdk.Mat(T_TCP_CMBase_np.tolist())
-T_grinderBase = rdk.Mat(T_grinderBase_np.tolist())
 T_pressBase = rdk.Mat(T_pressBase_np.tolist())
 T_intermediatePortafilter = rdk.Mat(T_intermediatePortafilter_np.tolist())
 T_grinderPortaPlace = rdk.Mat(T_grinderPortaPlace_np.tolist())
